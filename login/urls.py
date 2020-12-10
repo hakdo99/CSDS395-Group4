1232accd339c15c122aff9a06566f9a0980525b3
sar172 <sar172@case.edu>
2020-12-10 13:58:36 -0800

    Routed Login completion to home page

--- login/urls.py
+++ login/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import  include, path
 from . import views
+from homepage.views import mainPage
 
 urlpatterns = [
     path('', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('forgot/', views.forgotPassword, name='forgotPassword'),
+    path('loggedin/', mainPage, name='log in transfer'),
 ]
