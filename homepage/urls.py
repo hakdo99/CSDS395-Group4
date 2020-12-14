a4346cc3b5ddcfc0491a5da84ff63d9da103b637
sar172 <sar172@case.edu>
2020-12-14 12:36:57 -0800

    website starts on login screen

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from login.views import user_login
 
 urlpatterns = [
     path('myrestaurant', views.restaurantView, name='restaurantView'),
-    path('', views.mainPage, name='mainPage'),
+    path('home', views.mainPage, name='mainPage'),
     path('addsect', views.addSection, name='addSection'),
     path('addvid', views.addVideo, name='addVideo'),
-    path('sections', views.getSections, name='getSections')
+    path('sections', views.getSections, name='getSections'),
+    path('', user_login, name='initialPage'),
 ]
