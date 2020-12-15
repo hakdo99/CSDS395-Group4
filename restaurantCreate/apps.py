c132446e041fbb390fdad7dd736270389fc492b7
sar172 <sar172@case.edu>
2020-12-15 00:58:33 -0800

    registration works?

--- restaurantCreate/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestaurantcreateConfig(AppConfig):
-    name = 'restaurantCreate'
--- restaurantCreate/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.resCreate, name='resCreate'),
-    path('menu/', views.menu, name= 'resMenu'),
-    path('location/', views.location, name='resLocate'),
-]
